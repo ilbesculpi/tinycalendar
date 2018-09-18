3f9a5e79f93961d68dac0970e69009567c08eb39
Ilbert Esculpi <ilbert.esculpi@gmail.com>
2018-09-18 17:57:41 -0400

    remove boilerplate code. added Home Page

---
+++ src/pages/home/Home.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './Home.css';
+
+class HomePage extends Component {
+
+    render() {
+        return (<div className="container-fluid">
+            <h1>Tiny Calendar</h1>
+        </div>);
+    }
+
+}
+
+export { HomePage };
