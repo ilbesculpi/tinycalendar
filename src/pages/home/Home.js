c82bfdd2ae195d2333788694f4161af44ac6201a
Ilbert Esculpi <ilbert.esculpi@gmail.com>
2018-09-18 21:10:23 -0400

    handle user interactions in CalendarControls component

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -4,17 +4,44 @@ import './Home.css';
 
 class HomePage extends Component {
 
+    constructor(props) {
+
+        super(props);
+
+        this.handleChange = this.handleChange.bind(this);
+
+        // set initial values
+        this.state = {
+            startDate: '2018-07-15',
+            numberOfDays: 90,
+            countryCode: 'US'
+        };
+
+    }
+
+    /**
+     * Handle value changes for the CalendarControls component.
+     * @param {*} values 
+     */
+    handleChange(values) {
+        console.log('handleChange()', values);
+        this.setState(values);
+    }
+
     render() {
         return (<div className="container-fluid">
             <h1>Tiny Calendar</h1>
             <div className="row">
                 <div className="col-sm-4">
-                    <CalendarControls />
+                    <CalendarControls startDate={ this.state.startDate }
+                        numberOfDays={ this.state.numberOfDays }
+                        countryCode={ this.state.countryCode }
+                        onChange={ this.handleChange } />
                 </div>
                 <div className="col-sm-8">
-                    <CalendarView startDate="2018-07-31"
-                        numberOfDays="120"
-                        countryCode="US" />
+                    <CalendarView startDate={ this.state.startDate }
+                        numberOfDays={ this.state.numberOfDays }
+                        countryCode={ this.state.countryCode } />
                 </div>
             </div>
         </div>);
